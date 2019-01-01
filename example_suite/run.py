e06cf9be17a3305371bf5ee05bab45b93b81bb60
Tatu Aalto <aalto.tatu@gmail.com>
2019-01-01 10:08:32 +0200

    Improved run.py example

--- example_suite/run.py
+++ example_suite/run.py
@@ -14,7 +14,10 @@ def clean_output():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser('Runner script to easy the usage Robot Framework command line. '
+                                     'Unifies the usage and helps integration with CI environments. '
+                                     'Build on top of Python argparse, but in practise any scripting '
+                                     'language is good fot this usage.\n')
     parser.add_argument('-B', '--browser', default='headlesschrome')
     parser.add_argument('-i', '--include', default=[])
     parser.add_argument('-L',  '--loglevel', default='INFO')
